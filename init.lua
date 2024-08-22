d64d3ecfd1b818162358b9db5f2e6c6b56a34508
thedb <tcj841@alumni.ku.dk>
2024-08-22 15:53:45 +0200

    fixed tab behaviour in c#

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 require("core.plugins")
 require("core.plugin_config")
 
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.tabstop = 4           -- Number of spaces that a <Tab> counts for
+vim.opt.shiftwidth = 4        -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 4       -- Number of spaces that a <Tab> counts for while editing
+
 vim.wo.number = true
 
 vim.keymap.set('n', '<c-s>', ':w<CR>')
